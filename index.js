5a177453cea6e51421869ffac9435a58e06058cb
chriskelemba <loverhxchris@gmail.com>
2024-03-12 12:48:11 +0300

    postman method works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const studentRoute = require("./routes/studentRoute");
 
-app.use("/api/vendor", vendorRoute);
+app.use("/api/student", studentRoute);
 
 app.listen(process.env.port || 4000, function() {
     console.log("Now listening for requests on: http://localhost:4000");
