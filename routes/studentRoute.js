774edd36117edeb39b74891b48f15aec5575f46f
chriskelemba <loverhxchris@gmail.com>
2024-03-13 11:41:35 +0300

    student model

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -2,6 +2,9 @@ const express = require("express");
 const studentController = require("../controller/studentController");
 const router = express.Router();
 
+router.get("/addStudent", studentController.addStudent);
 router.post("/addStudent", studentController.addStudent);
+router.put("/addStudent", studentController.addStudent);
+router.delete("/addStudent", studentController.addStudent);
 
 module.exports = router;
