c39ee69a1e27e675e6a102cf555da407e9765a10
chriskelemba <loverhxchris@gmail.com>
2024-03-15 13:00:55 +0300

    git repository fixed

--- controller/studentController.js
+++ controller/studentController.js
@@ -3,6 +3,7 @@ const db = require("../model/dbConnect");
 const student = db.student;
 
 module.exports = {
+    // Add Student
     addStudent: async(req, res, next) => {
         try {
             let info = {
@@ -14,6 +15,30 @@ module.exports = {
             const addStudent = await student.create(info)
 
             res.status(200).send(addStudent)
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    // Get All Students
+    getAllStudent: async(req, res, next) => {
+        try {
+            let students = await student.findAll({})
+            res.status(200).send(students)
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    // Get Student by ID
+    getStudent: async(req, res, next) => {
+        try {
+            let id = req.params.student_id
+            let student = await student.findOne({where: {student_id: id}})
+
+            if(!student) {
+                throw(createError(404, "Student does not exist."))
+            }
         } catch (error) {
             next(error)
         }
